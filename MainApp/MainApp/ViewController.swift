d54d61a13b40475d1f50f01cfe7ee561d75125d1
Tiara Mahardika <tiara.mahardika@digitalcenter.id>
2022-06-07 22:58:28 +0700

    add project

---
+++ MainApp/MainApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MainApp
+//
+//  Created by Hario Budiharjo on 07/06/22.
+//
+
+import UIKit
+import CalsSDK
+
+class ViewController: UIViewController {
+    CalsSDK.Tes
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Tes.keluar()
+    }
+
+
+}
+
---
+++ MainApp/Pods/Target Support Files/CalsSDK/CalsSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CalsSDK : NSObject
+@end
+@implementation PodsDummy_CalsSDK
+@end
